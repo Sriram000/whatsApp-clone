6d2724bab4e678436d0fc8ad1323dc5741fcb38d
sriram000 <srirammani14@gmail.com>
2022-03-20 15:30:17 +0530

    Initialized.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxsSJIShI1rKO23PxIUyQVlavXkkfaW40",
+    authDomain: "whatsapp-clone-c9907.firebaseapp.com",
+    projectId: "whatsapp-clone-c9907",
+    storageBucket: "whatsapp-clone-c9907.appspot.com",
+    messagingSenderId: "25606161925",
+    appId: "1:25606161925:web:46f19492551e8cca023e6c"
+  };
